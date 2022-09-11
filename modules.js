b4e6fac914d95c2d17e98bc88affdafeed686e5f
YUVRAJ6336 <193116@nith.ac.in>
2022-09-12 01:08:37 +0530

    first commit

---
+++ modules.js
@@ -0,0 +1,36 @@
+// const number = 12
+
+// if(number < 10){
+//     console.log('small number');
+// }
+
+// else{
+//     console.log('big number')
+// }
+
+// console.log(`hey it's my first node app !!!`)
+// console.log(__dirname)
+// setInterval(() =>{
+//     console.log('hello yuvraj')
+// },1000)
+
+// const john = 'john'
+// const peter = 'peter'
+// const secret = 'SUPER SECRET'
+
+
+// const sayHi = (name) =>{
+//     console.log(`Hello there ${name}`)
+// }
+
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./altenative-flavor')
+require('./mind-grenade')
+
+
+
+// sayHi('Nsr');
+// sayHi(names.john);
+// sayHi(names.peter);
+// sayHi(secret);
---
+++ names.js
@@ -0,0 +1,7 @@
+//local
+const john = 'john'
+//share or global
+const peter = 'peter'
+const secret = 'SUPER SECRET'
+
+module.exports = { john,peter }
